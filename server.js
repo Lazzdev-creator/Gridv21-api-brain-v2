ecc630956d74bb9d261db52d822c4656d437ce27
Lazzdev-creator <ltchenana.thirteen@gmail.com>
2026-07-27 08:46:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -25,9 +25,6 @@ import crypto from "crypto";
 import { createClient as createRedisClient } from "redis";
 import { RedisStore } from "connect-redis";
 
-import path from 'path';
-import { fileURLToPath } from 'url';
-
 // Define __dirname for ES Modules (Node.js)
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
